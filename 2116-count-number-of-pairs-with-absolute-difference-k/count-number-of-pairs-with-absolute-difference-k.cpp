0ad1c2e0d1710c1ee312663c76eb48d9c36af3f4
Minhaj078 <152924478+Minhaj078@users.noreply.github.com>
2025-03-06 02:50:48 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (40.07%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        int count = 0;
+        // unordered_map<int, int> freq;
+        // int count = 0;
         
-        // Loop through each element in the vector
-        for (int num : nums) {
-            // Agar (num - k) pehle se exist karta hai, to un pairs ko add karo
-            if (freq.find(num - k) != freq.end()) {
-                count += freq[num - k];
-            }
-            // Agar (num + k) pehle se exist karta hai, to un pairs ko add karo
-            if (freq.find(num + k) != freq.end()) {
-                count += freq[num + k];
-            }
+        // // Loop through each element in the vector
+        // for (int num : nums) {
+        //     // Agar (num - k) pehle se exist karta hai, to un pairs ko add karo
+        //     if (freq.find(num - k) != freq.end()) {
+        //         count += freq[num - k];
+        //     }
+        //     // Agar (num + k) pehle se exist karta hai, to un pairs ko add karo
+        //     if (freq.find(num + k) != freq.end()) {
+        //         count += freq[num + k];
+        //     }
             
-            // Update frequency of current element
-            freq[num]++;
+        //     // Update frequency of current element
+        //     freq[num]++;
+        // }
+        // return count;
+
+    int freq[101] = {0};  // Since 1 <= nums[i] <= 100
+    int count = 0;
+
+    // Step 1: Count frequency of each number (O(N))
+    for (int num : nums) {
+        freq[num]++;
+    }
+
+    // Step 2: Count valid pairs (O(N))
+    for (int i = 1; i <= 100; i++) {
+        if (i + k <= 100) {
+            count += freq[i] * freq[i + k];
         }
-        return count;
+    }
+
+    return count;
     }
 };
