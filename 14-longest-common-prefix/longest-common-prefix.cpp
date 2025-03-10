f356e29affc5f19cf5ca72fd2027b3852aa2c584
Minhaj078 <152924478+Minhaj078@users.noreply.github.com>
2025-03-10 13:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (86.27%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        // string ans = "";
+        // int n = strs.size();
+        // for(int i = 0;i<strs[0].length();i++){
+        //     char ch = strs[0][i];
+
+        //     bool match = true;
+        //     for(int j= 1;j<n;j++){
+        //         if(strs[j].size() < i || ch!= strs[j][i]){
+        //             match = false;
+        //             break;
+        //         }
+        //     }
+        //     if(match == false)break;
+        //     else{
+        //         ans.push_back(ch);
+        //     }
+        // }
+        // return ans;
+
         string ans = "";
         int n = strs.size();
-        for(int i = 0;i<strs[0].length();i++){
+        for(int i =0;i<strs[0].length();i++){
             char ch = strs[0][i];
 
-            bool match = true;
-            for(int j= 1;j<n;j++){
-                if(strs[j].size() < i || ch!= strs[j][i]){
-                    match = false;
+            bool flag = true;
+            for(int j = 1;j<n;j++){
+                if(strs[j][i] != ch){
+                    flag = false;
                     break;
                 }
             }
-            if(match == false)break;
+            if(flag == false)break;
             else{
                 ans.push_back(ch);
             }
-        }
-        return ans;
+        }return ans;
     }
 };
