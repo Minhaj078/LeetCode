311cb55b2581c37697a8c6b7949f7ef9b443e86e
Minhaj078 <152924478+Minhaj078@users.noreply.github.com>
2025-03-18 15:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (82.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
+    typedef pair<int,int>r;
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<r>q;
+        //vector<vector<int>> vis(n, vector<int>(m, 0));
 
         int cntfresh = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j] == 2){
-                    q.push({{i,j},0});
-                    vis[i][j] = 2; // visited the rotten apple in visisted array
-                }else{
-                    vis[i][j] = 0; // rather there will be empty space or fresh apple
-                }
-                if(grid[i][j] == 1)cntfresh++; //got that fresh orange
+                    q.emplace(i,j);
+                    //vis[i][j] = 2; // visited the rotten apple in visisted array
+                }else if(grid[i][j] == 1)cntfresh++; //got that fresh orange
             }
         }
-
-        int tm = 0,rotted = 0;
+        if(cntfresh==0){
+            return 0;
+        }
+        int time = 0;
         //for calculating my neighbours make an array for up down left right;
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
+        constexpr int dirs[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-
-            tm = max(t,tm);
+            int p=q.size();
+            while(p--){
+            r node=q.front();
             q.pop();
-
-            for(int i = 0;i<4;i++){
-                int nrow = r+drow[i];
-                int ncol = c+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                q.push({{nrow,ncol},t+1});
-                vis[nrow][ncol] = 2;
-                rotted++;
+            int i=node.first;
+            int j=node.second;
+                for(auto& [dx,dy]:dirs){
+                    int nrow=i+dx;
+                    int ncol=j+dy;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1){
+                grid[nrow][ncol] = 2;
+                q.emplace(nrow,ncol);
+                cntfresh--;
             }
             }
-        }return cntfresh == rotted ? tm:-1;
+            }
+            time++;
+        }
+        return cntfresh == 0?time-1:-1;
     }
 };
