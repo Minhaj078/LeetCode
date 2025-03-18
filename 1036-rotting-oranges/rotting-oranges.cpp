4edde363d17011bfeb17fe43d0449464997ae9fb
Minhaj078 <152924478+Minhaj078@users.noreply.github.com>
2025-03-18 14:11:07 +0530

    Time: 3 ms (30.25%) | Memory: 17.1 MB (41.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,57 +1,47 @@
-#include <vector>
-#include <queue>
-using namespace std;
-
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<pair<int, int>, int>> q;
+        queue<pair<pair<int,int>,int>>q;
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        int cntFresh = 0;
 
-        // Initialize queue with all initially rotten oranges
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                } else {
-                    vis[i][j] = 0;
+        int cntfresh = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j},0});
+                    vis[i][j] = 2; // visited the rotten apple in visisted array
+                }else{
+                    vis[i][j] = 0; // rather there will be empty space or fresh apple
                 }
-                if (grid[i][j] == 1) cntFresh++;
+                if(grid[i][j] == 1)cntfresh++; //got that fresh orange
             }
         }
 
-        int tm = 0, rotted = 0;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
+        int tm = 0,rotted = 0;
+        //for calculating my neighbours make an array for up down left right;
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
 
-        while (!q.empty()) {
+        while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
 
-            tm = max(tm, t);
+            tm = max(t,tm);
             q.pop();
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
-                    q.push({{nrow, ncol}, t + 1});
-                    vis[nrow][ncol] = 2;
-                    rotted++; // Increment count of rotted oranges
-                }
+            for(int i = 0;i<4;i++){
+                int nrow = r+drow[i];
+                int ncol = c+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                q.push({{nrow,ncol},t+1});
+                vis[nrow][ncol] = 2;
+                rotted++;
             }
-        }
-
-        // If not all fresh oranges turned rotten, return -1
-        return (rotted == cntFresh) ? tm : -1;
+            }
+        }return cntfresh == rotted ? tm:-1;
     }
 };
