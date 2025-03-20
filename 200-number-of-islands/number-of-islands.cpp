7250cb4829c8a14eddd2ce4c688985ebae95317b
Minhaj078 <152924478+Minhaj078@users.noreply.github.com>
2025-03-20 16:37:45 +0530

    Time: 28 ms (51.25%) | Memory: 17.2 MB (53.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,36 +1,75 @@
 class Solution {
 public:
 
-    void bfs(int i,int j,vector<vector<int>>&vis,vector<vector<char>>&grid){
-        vis[i][j] = 1;
-        queue<pair<int,int>>q;
-        q.push({i,j});
+    // void bfs(int i,int j,vector<vector<int>>&vis,vector<vector<char>>&grid){
+    //     vis[i][j] = 1;
+    //     queue<pair<int,int>>q;
+    //     q.push({i,j});
 
-        int n = grid.size();
-        int m = grid[0].size();
+    //     int n = grid.size();
+    //     int m = grid[0].size();
 
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
+    //     while(!q.empty()){
+    //         int r = q.front().first;
+    //         int c = q.front().second;
+    //         q.pop();
 
-            int delrow [] = {-1,0,1,0};
-            int delcol [] = {0,1,0,-1};
-            for(int i = 0;i<4;i++)
-            {
-                    int nrow = r + delrow[i];
-                    int ncol = c + delcol[i];
+    //         int delrow [] = {-1,0,1,0};
+    //         int delcol [] = {0,1,0,-1};
+    //         for(int i = 0;i<4;i++)
+    //         {
+    //                 int nrow = r + delrow[i];
+    //                 int ncol = c + delcol[i];
 
-                    if(nrow >= 0 && nrow< n && ncol >=0 && ncol<m && grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
-                        vis[nrow][ncol] = 1;
-                        q.push({nrow,ncol});
-                    }
+    //                 if(nrow >= 0 && nrow< n && ncol >=0 && ncol<m && grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
+    //                     vis[nrow][ncol] = 1;
+    //                     q.push({nrow,ncol});
+    //                 }
                 
-            }
+    //         }
+    //     }
+    // }
+
+    // int numIslands(vector<vector<char>>& grid) {
+    //     int n = grid.size();
+    //     int m = grid[0].size();
+
+    //     int cnt = 0;
+    //     vector<vector<int>>vis(n,vector<int>(m,0));
+
+    //     for(int i = 0;i<n;i++){
+    //         for(int j = 0;j<m;j++){
+    //             if(!vis[i][j] && grid[i][j] == '1'){
+    //                 bfs(i,j,vis,grid);
+    //                 cnt++;
+    //             }
+    //         }
+    //     }return cnt;
+    // }
+
+    //dfs
+    
+    void dfs(int i,int j,vector<vector<int>>&vis,vector<vector<char>>&grid){
+        vis[i][j] = 1;
+
+        int n = grid.size();       
+        int m = grid[0].size();       
+
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+
+        for(int k = 0;k<4;k++){
+
+            int nrow = i + drow[k];
+            int ncol = j + dcol[k];
+
+            if(nrow >=0 && nrow<n && ncol >=0 && ncol< m && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0){
+                dfs(nrow,ncol,vis,grid);
+            } 
         }
     }
 
-    int numIslands(vector<vector<char>>& grid) {
+        int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
@@ -40,7 +79,7 @@ public:
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
-                    bfs(i,j,vis,grid);
+                    dfs(i,j,vis,grid);
                     cnt++;
                 }
             }
