4e1aa2690e4a8281426f9559a822eabcb4dcc5bd
Minhaj078 <152924478+Minhaj078@users.noreply.github.com>
2025-04-20 14:06:08 +0530

    Time: 6 ms (57.06%) | Memory: 19.5 MB (59.73%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-
-    bool detect(int node,vector<int>&path,vector<vector<int>>& adj,vector<int>&vis){
-        vis[node] = 1;
-        path[node] = 1;
-
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(detect(it,path,adj,vis))return true;
-            }
-            else if(path[it])return true;
-        }
-        path[node] = 0;
-        return false;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
+        vector<int>adj[numCourses];
         for (auto it : prerequisites) {
-            adj[it[1]].push_back(it[0]);  // it[1] → it[0]
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int>path(numCourses,0);
-        vector<int>vis(numCourses,0);
-
+        vector<int> indegree(numCourses, 0);
+        for (int i = 0; i < numCourses; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        int cnt = 0;
         for(int i = 0;i<numCourses;i++){
-            if(!vis[i]) if(detect(i,path,adj,vis))return false; //mtlb ki hn bhai cycle hai tm course ni kr paoge khtm ..
+            if(indegree[i] == 0)q.push(i);
         }
-        return true;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0)q.push(it);
+            }
+        }
+        return cnt == numCourses;
     }
 };
