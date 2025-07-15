4447f79e8896ed6459d535dc14a5ea8606593f4c
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-07-16 02:56:24 +0530

    Time: 3 ms (11.63%) | Memory: 8.1 MB (33.95%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,24 +1,54 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         int n = s.length();
+//         if (n < 3) return false;
+
+//         int vowels = 0, consonants = 0;
+//         string vowelList = "aeiouAEIOU";
+
+//         for (char c : s) {
+//             if (isalpha(c)) {
+//                 if (vowelList.find(c) != string::npos) {
+//                     vowels++;
+//                 } else {
+//                     consonants++;
+//                 }
+//             } else if (!isdigit(c)) {
+//                 return false; // invalid character
+//             }
+//         }
+
+//         return vowels >= 1 && consonants >= 1;
+//     }
+// };
+
+
 class Solution {
 public:
-    bool isValid(string s) {
-        int n = s.length();
-        if (n < 3) return false;
-
-        int vowels = 0, consonants = 0;
-        string vowelList = "aeiouAEIOU";
-
-        for (char c : s) {
-            if (isalpha(c)) {
-                if (vowelList.find(c) != string::npos) {
-                    vowels++;
-                } else {
-                    consonants++;
+    bool isValid(string word) {
+        //word="3ab"
+        int vov_cnt=0;
+        int con_cnt=0;
+        int num=0;
+        for(char i:word){
+            if(isalpha(i) || isdigit(i)){
+                if(isdigit(i))num++; //num=1;
+                else{
+                    if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u' || i == 'A' || i == 'E' || i == 'I' || i == 'O' || i == 'U'){
+                        vov_cnt++; //vov_cnt=1;
+                    }else{
+                        con_cnt++; //con_cnt=1;
+                    }
                 }
-            } else if (!isdigit(c)) {
-                return false; // invalid character
+            }else{
+                return false;
             }
         }
-
-        return vowels >= 1 && consonants >= 1;
+        if(vov_cnt>=1 && con_cnt>=1 && vov_cnt+con_cnt+num>=3){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
