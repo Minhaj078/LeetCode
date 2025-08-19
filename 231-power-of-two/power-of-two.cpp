523e580c89365eddd039927bfcc745992f4c6b9f
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-08-20 01:11:05 +0530

    Time: 2 ms (5.41%) | Memory: 7.8 MB (83.57%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        for (int i = 0; i <= 31; i++) {
+        if ((1LL << i) == n) return true;
+    }
+    return false;
     }
 };
