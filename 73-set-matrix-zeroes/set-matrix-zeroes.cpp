61f2ac20e338e97b7bb08247a5b5c9c67269dff4
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-08-21 00:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (95.76%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int n = matrix.size(), m = matrix[0].size();
+        bool fr = false, fc = false;
 
-        vector<vector<int>>visited(n,vector<int>(m,1));
+        // check first row & first column
+        for (int j = 0; j < m; j++) if (matrix[0][j] == 0) fr = true;
+        for (int i = 0; i < n; i++) if (matrix[i][0] == 0) fc = true;
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(matrix[i][j] == 0)visited[i][j] = 0;
-            }
-        }
+        // mark zeros in first row & col
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < m; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(visited[i][j] == 0){
-                    for(int k = 0;k<n;k++){
-                        matrix[k][j] = 0;
-                    }
+        // update cells
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < m; j++)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
 
-                    for(int k = 0;k<m;k++){
-                        matrix[i][k] = 0;
-                    }
-                }
-            }
-        }
-        
+        // update first row & column if needed
+        if (fr) for (int j = 0; j < m; j++) matrix[0][j] = 0;
+        if (fc) for (int i = 0; i < n; i++) matrix[i][0] = 0;
     }
 };
