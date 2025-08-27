4cd6ca6a27237267304314f62fa4d5524c813610
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-08-27 19:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (29.03%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
 
-    int fn(int i,int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(i >=0 && j >=0 && grid[i][j] == 1)return 0; //obstacle found
-        if(i == 0 && j == 0) return 1; //sirf starting ko hi return kro 1
-        if(i<0 || j <0)return 0; //out of the boundry
+    int fn(int i, int j, vector<vector<int>>&dp, vector<vector<int>>&arr){
+
+        if(i >= 0 && j >= 0 && arr[i][j] == 1)return 0;
+
+        if(i < 0 || j < 0)return 0;
+
+        if(i == 0 && j == 0)return 1;
 
         if(dp[i][j] != -1)return dp[i][j];
 
-        int left = fn(i,j-1,grid,dp);
-        int top = fn(i-1,j,grid,dp);
+        int left = fn(i, j-1, dp, arr);
+        int up = fn(i-1, j, dp, arr);
 
-        return dp[i][j] = left + top;
+        return dp[i][j] = left + up;
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return fn(n-1,m-1,obstacleGrid,dp);
+
+        return fn(n-1,m-1,dp,obstacleGrid);
     }
 };
