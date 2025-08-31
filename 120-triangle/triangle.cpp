9d052f5408f5df02163fe98212122dfdd06fa7da
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-08-31 18:07:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (31.45%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-
-
-    int fn(int i, int j, vector<vector<int>>&dp, vector<vector<int>>& arr,int n){
-        if(i == n-1)return arr[n-1][j];
-
-        if(dp[i][j] != -1)return dp[i][j];
-
-        int d = arr[i][j] + fn(i+1,j,dp,arr,n);
-        int dg = arr[i][j] + fn(i+1,j+1,dp,arr,n);
-
-        return dp[i][j] = min(d,dg);
-
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
 
-        //This memoization will give us TLE;
-        // vector<vector<int >>dp(n,vector<int>(n,-1));
-        // return fn(0,0,dp,triangle,n);
+        int n = triangle.size();
 
         vector<vector<int >>dp(n,vector<int>(n,0));
 
-        for(int j = 0;j<n;j++) dp[n-1][j] = triangle[n-1][j];
-        for(int i = n-2; i>=0; i--){
-            for(int j = i; j>=0; j--){
-                int d = triangle[i][j] + dp[i+1][j];
-                int dg = triangle[i][j] + dp[i+1][j+1];
+        for(int j = 0;j<n;j++) dp[n-1][j] = triangle[n-1][j]; //last row bhr do
+
+        for(int i = n-2;i>=0;i--){
+            for(int j = i;j>=0;j--){
+                int d = triangle[i][j] + dp[i+1][j]; //basically ik row mai check kr ke likalo 
+                int dg = triangle[i][j] + dp[i+1][j+1]; // and diagonal wise dekh lo
+
                 dp[i][j] = min(d,dg);
             }
-        }
-
-        return dp[0][0];
+        }return dp[0][0];
 
     }
 };
