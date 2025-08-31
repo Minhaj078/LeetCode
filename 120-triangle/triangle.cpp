c7c3d9eeaf9a6b48d163b905b9c4f7a554ec2e6e
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-08-31 16:42:28 +0530

    Time: 3 ms (14.40%) | Memory: 13.3 MB (47.71%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int fn(int i, int j, vector<vector<int>>&dp, vector<vector<int>>& arr,int n){
+        if(i == n-1)return arr[n-1][j];
+
+        if(dp[i][j] != -1)return dp[i][j];
+
+        int d = arr[i][j] + fn(i+1,j,dp,arr,n);
+        int dg = arr[i][j] + fn(i+1,j+1,dp,arr,n);
+
+        return dp[i][j] = min(d,dg);
+
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        //This memoization will give us TLE;
+        // vector<vector<int >>dp(n,vector<int>(n,-1));
+        // return fn(0,0,dp,triangle,n);
+
+        vector<vector<int >>dp(n,vector<int>(n,0));
+
+        for(int j = 0;j<n;j++) dp[n-1][j] = triangle[n-1][j];
+        for(int i = n-2; i>=0; i--){
+            for(int j = i; j>=0; j--){
+                int d = triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(d,dg);
+            }
+        }
+
+        return dp[0][0];
+
+    }
+};
