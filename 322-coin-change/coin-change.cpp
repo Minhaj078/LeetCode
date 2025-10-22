2e56972f55f450eeea510943177d2f5ae310e75f
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-10-22 16:52:46 +0530

    Time: 47 ms (35.15%) | Memory: 49.3 MB (32.79%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int fn(int i, int a, vector<vector<int>>& dp, vector<int>& coins) {
-        // base case
-        if (i == 0) {
-            if (a % coins[0] == 0) return a / coins[0];
-            return 1e9;
+
+    int fn(int n, int a, vector<int>&coins, vector<vector<int>> &dp){
+
+        if(n == 0){
+           return (a % coins[0] == 0) ? a/coins[0] : 1e9;
         }
 
-        if (dp[i][a] != -1) return dp[i][a];
+        if(dp[n][a] != -1)return dp[n][a];
 
-        int notTake = 0+fn(i - 1, a, dp, coins);
+        int notTake = 0 + fn(n-1, a, coins, dp);
         int take = 1e9;
 
-        if (coins[i] <= a) {
-            take = 1 + fn(i, a - coins[i], dp, coins);
-        }
+        if(a >= coins[n]) take = 1 + fn(n, a - coins[n], coins, dp);
 
-        return dp[i][a] = min(take, notTake);
+        return dp[n][a] = min(take, notTake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = fn(n - 1, amount, dp, coins);
-        return (ans != 1e9) ? ans : -1;
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1, -1));
+        int ans = fn(coins.size()-1, amount, coins, dp);
+        return (ans == 1e9) ? -1 : ans;
+
     }
 };
