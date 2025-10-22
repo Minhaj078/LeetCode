2939522125995cf8a69cb8d49c3803b91fec568c
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-10-22 15:25:07 +0530

    Time: 2095 ms (5.00%) | Memory: 471.9 MB (5.56%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+
+        for(int i = 0;i<n;i++){
+            unordered_set<int>seen;
+            if(i > 0 && nums[i] == nums[i-1])continue;
+
+            for(int j = i+1;j<n;j++){
+                
+                int third = -(nums[i] + nums[j]);
+                if(seen.count(third))
+                {
+
+                ans.push_back({nums[i],third,nums[j]});
+
+                while(j+1 < n && nums[j] == nums[j+1])j++;
+
+                }
+                seen.insert(nums[j]);
+            }
+            
+        }
+        return ans;
+    }
+};
