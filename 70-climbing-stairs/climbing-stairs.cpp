ff143ce30eab740ad284ccad88aa95bc64039fd7
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-10-29 19:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int fn(int n, vector<int> &dp){
-        if(n == 0) return 1;
-        if(n == 1) return 1;
 
-        if(dp[n] != -1) return dp[n]; // pehle se calculate hai
+    int fn (int n, vector<int>&dp){
+        if(n == 0)return 1;
+        if(n < 0)return 0;
 
-        return dp[n] = fn(n-1, dp) + fn(n-2, dp);
+        if(dp[n]!= -1)return dp[n];
+
+        int step1 = fn(n-1, dp);
+        int step2 = fn(n-2, dp);
+        return dp[n] =  step1 + step2;
+
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
+        vector<int>dp(n+1,-1);
         return fn(n, dp);
     }
 };
