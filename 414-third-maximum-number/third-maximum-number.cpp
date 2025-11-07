828b43e15b6c17ee429295f3e4049cf3307860f9
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-11-07 19:53:00 +0530

    Time: 5 ms (19.08%) | Memory: 14.6 MB (8.14%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        // store unique elements (sorted ascending)
-        set<int> s(nums.begin(), nums.end());
-        
-        // if less than 3 unique numbers, return max
-        if (s.size() < 3)
-            return *s.rbegin(); // largest element
-        
-        // otherwise, return third maximum
-        auto it = s.rbegin(); // reverse iterator (largest → smallest)
-        advance(it, 2); // move to 3rd largest
-        return *it;
+        set<int, greater<int>> s(nums.begin(), nums.end());
+        vector<int> v(s.begin(), s.end());
+
+        if (v.size() < 3)
+            return v[0]; // largest
+        return v[2];     // 3rd largest
     }
 };
