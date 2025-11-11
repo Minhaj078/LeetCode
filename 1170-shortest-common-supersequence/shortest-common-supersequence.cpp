ab8df24d26c74381b16b9ef6a0f4bf45452e36c1
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-11-12 01:30:44 +0530

    Time: 25 ms (58.04%) | Memory: 26.6 MB (88.56%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,55 +1,51 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int >> dp(n+1,vector<int>(m+1,0));
 
-        // Create DP table
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= m;j++){
+                if(str1[i-1] == str2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
 
-        // Fill the DP table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1; // If characters match
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); // Take max from either previous row or column
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
 
-        // Now build the shortest common supersequence
-        string result = "";
-        int i = m, j = n;
-        while (i > 0 && j > 0) {
-            // If characters are the same, add to the result
-            if (str1[i - 1] == str2[j - 1]) {
-                result += str1[i - 1];
+        string ans = "";
+        int i = n, j = m;
+
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans += str1[i-1];
                 i--;
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) { // Move in the direction of larger dp value
-                result += str1[i - 1];
+            }
+
+            else if(dp[i-1][j] > dp[i][j-1]){
+                ans += str1[i-1];
                 i--;
-            } else {
-                result += str2[j - 1];
+            }
+            else{
+                ans += str2[j-1];
                 j--;
             }
         }
 
-        // If there are any remaining characters in str1
-        while (i > 0) {
-            result += str1[i - 1];
+        while(i > 0){
+            ans += str1[i-1];
             i--;
         }
 
-        // If there are any remaining characters in str2
-        while (j > 0) {
-            result += str2[j - 1];
+        while(j > 0){
+            ans += str2[j-1];
             j--;
         }
 
-        // Since we added characters in reverse order, reverse the result before returning
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
