def32db0ad80911afdc32ad82b9316b0d3341365
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-11-20 22:05:10 +0530

    Time: 4 ms (31.18%) | Memory: 15.8 MB (31.97%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-
-    int fn(vector<int>& nums,int &original){
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == original)return (nums[i] * 2);
-        }
-        return original;    
-    }
-
     int findFinalValue(vector<int>& nums, int original) {
-        int ans = original;
-        for(int i = 0;i<nums.size();i++){
-            ans = fn(nums,original);
-            original = ans;
-        }return ans;
+        unordered_set<int> st(nums.begin(), nums.end());
+        
+        while (st.count(original)) {
+            original *= 2;
+        }
+
+        return original;
     }
 };
