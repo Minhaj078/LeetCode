6b57f1d1c3e6ed05fc60d577d2936352442cc1d1
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-12-24 22:50:35 +0530

    Time: 21 ms (57.42%) | Memory: 17.4 MB (82.19%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -33,11 +33,13 @@ public:
                             {nums[i], nums[j], nums[left], nums[right]}
                         );
 
+                        
+
+                        while(left < right && nums[left] == nums[left + 1]) left++;
+                        while(left < right && nums[right] == nums[right - 1]) right--;
+
                         left++;
                         right--;
-
-                        while(left < right && nums[left] == nums[left - 1]) left++;
-                        while(left < right && nums[right] == nums[right + 1]) right--;
                     }
                     else if(sum < target) {
                         left++;
