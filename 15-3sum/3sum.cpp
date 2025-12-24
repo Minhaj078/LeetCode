e49148000a07996b9e6fe6083e0760760a82b586
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-12-24 23:00:50 +0530

    Time: 55 ms (33.20%) | Memory: 29.2 MB (45.81%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -14,10 +14,12 @@ public:
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
                     ans.push_back({nums[i],nums[left],nums[right]});
-                    while(left < right && nums[left] == nums[left+1])left++; // check for duplicates
-                    while(left < right && nums[right] == nums[right - 1])right--; // check for duplicates
                     left++;
                     right--;
+                    
+                    while(left < right && nums[left] == nums[left - 1])left++; // check for duplicates
+                    while(left < right && nums[right] == nums[right + 1])right--; // check for duplicates
+                    
                 }
                 else if(sum < 0)left++;
                 else{
