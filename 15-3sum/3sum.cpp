f27a984bc3b14c5bd4f520d8cc554dba42d80786
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2026-01-11 22:14:33 +0530

    Time: 41 ms (87.42%) | Memory: 29.1 MB (45.40%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,30 +2,36 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        for(int i = 0;i<n; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+        vector<vector<int>> ans;
 
-            int left = i + 1, right = n - 1;
+        for(int i = 0; i < n - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while(left < right){
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
-                    ans.push_back({nums[i],nums[left],nums[right]});
+
+                if(sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
-                    
-                    while(left < right && nums[left] == nums[left - 1])left++; // check for duplicates
-                    while(left < right && nums[right] == nums[right + 1])right--; // check for duplicates
-                    
+
+                    // skip duplicates
+                    while(left < right && nums[left] == nums[left - 1]) left++;
+                    while(left < right && nums[right] == nums[right + 1]) right--;
                 }
-                else if(sum < 0)left++;
-                else{
+                else if(sum < 0) {
+                    left++;
+                }
+                else {
                     right--;
                 }
             }
-        }return ans;
+        }
+        return ans;
     }
 };
