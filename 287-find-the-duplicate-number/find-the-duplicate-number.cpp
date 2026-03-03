d370f558ae10fafcc9e59eb95e278fee8ab6c7af
Minhajuddin Ahmad <152924478+Minhaj078@users.noreply.github.com>
2026-03-03 23:21:29 +0530

    Time: 4 ms (60.76%) | Memory: 65.2 MB (40.56%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,22 +4,18 @@ public:
         int slow = nums[0];
         int fast = nums[0];
 
-        while (true) {
+        while(true){
             slow = nums[slow];
             fast = nums[nums[fast]];
 
-            if (slow == fast) {
-                break;
-            }
+            if(slow == fast)break;
         }
 
-        int slow2 = nums[0];
-
-        while (slow != slow2) {
+        slow = nums[0];
+        while(slow != fast){
             slow = nums[slow];
-            slow2 = nums[slow2];
+            fast = nums[fast];
         }
-
-        return slow;        
+        return slow;
     }
 };
