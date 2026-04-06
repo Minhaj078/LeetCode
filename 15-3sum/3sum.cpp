13d228b71c3e8e5431eda0697612e09c25ad7fa0
Minhajuddin Ahmad <152924478+Minhaj078@users.noreply.github.com>
2026-04-07 00:51:13 +0530

    Time: 54 ms (30.86%) | Memory: 28.9 MB (99.51%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,27 +4,28 @@ public:
         vector<vector<int>>ans;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-
-        for(int i = 0;i<n; i++){
+        for(int i = 0;i<n;i++){
             if(i > 0 && nums[i] == nums[i-1])continue;
 
-            int l = i + 1, r = n-1;
-            while(l<r){
-                int sum = nums[i] + nums[l] + nums[r];
+            int left = i+1, right = n-1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
-                    ans.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
 
-                    while(l < r && nums[l] == nums[l-1])l++;
-                    while(l < r && nums[r] == nums[r+1])r--;
+
+                    while(left < right && nums[left] == nums[left-1])left++;
+                    while(left < right && nums[right] == nums[right + 1])right--;
                 }
-                else if(sum < 0)l++;
+
+                else if(sum > 0)right--;
                 else{
-                    r--;
+                    left++;
+                }
                 }
             }
-        }
         return ans;
-    }
+     }
 };
