f545f059cd74232a52a03717adacb99b1a1639c9
Minhajuddin Ahmad <152924478+Minhaj078@users.noreply.github.com>
2026-05-26 03:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (94.82%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
+        int noDel = arr[0], oneDel = 0, ans = arr[0];
 
-        int noDel = arr[0];
-        int oneDel = 0;
-        int ans = arr[0];
-
-        for(int i = 1; i < arr.size(); i++) {
-
+        for(int i = 1; i<arr.size(); i++){
             int temp = noDel;
 
-            noDel = max(arr[i], noDel + arr[i]);
+            noDel = max(arr[i], noDel + arr[i]); // basic kadaneAlgo
 
             oneDel = max(temp, oneDel + arr[i]);
 
-            ans = max(ans, max(noDel, oneDel));
-        }
-
-        return ans;
+            ans = max(ans, max(noDel , oneDel));
+        }return ans;
     }
 };
